f458f038136427e20be7ef160598602ffd258fd4
Pireu <65513625+Pireu@users.noreply.github.com>
2020-05-24 19:00:56 -0300

    Leitura dos dados

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,9 +1,22 @@
 
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string sexo;
+    int cerveja, refri, espeto;
+
+    cout << "Sexo: ";
+    cin >> sexo;
+    cout << "Quantidade de cervejas: ";
+    cin >> cerveja; 
+    cout << "Quantidade de refrigerantes: ";
+    cin >> refri;
+    cout << "Quantidade de espetinhos: ";
+    cin >> espeto;
+
+
 }
 
