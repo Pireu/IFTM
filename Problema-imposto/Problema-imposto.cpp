34126f104b7efbab19d6268b3e4762b489295382
Pireu <65513625+Pireu@users.noreply.github.com>
2020-05-25 11:58:19 -0300

    Leitura dos dados

--- Problema-imposto/Problema-imposto.cpp
+++ Problema-imposto/Problema-imposto.cpp
@@ -1,9 +1,23 @@
-
 #include <iostream>
+#include <bits/stdc++.h>
+#include <stdio.h>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double ganhoSalario, ganhoServicos, ganhoCapital, gastoMedico, gastoEducacao;
+
+    cout << "Renda anual com salário: ";
+    cin >> ganhoSalario;
+    cout << "Renda anual com prestação de serviço: ";
+    cin >> ganhoServicos;
+    cout << "Renda anual com ganho de capital: ";
+    cin >> ganhoCapital;
+    cout << "Gastos médicos: ";
+    cin >> gastoMedico;
+    cout << "Gastos educacionais: ";
+    cin >> gastoEducacao;
+
+    return 0;
 }
-
-
