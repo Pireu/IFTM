0d9e2ddd4b0fee9c0875eb22a774a27bd550ba57
Pireu <65513625+Pireu@users.noreply.github.com>
2020-05-27 17:10:20 -0300

    Consolidado de renda

--- Problema-imposto/Problema-imposto.cpp
+++ Problema-imposto/Problema-imposto.cpp
@@ -1,23 +1,53 @@
 #include <iostream>
-#include <bits/stdc++.h>
 #include <stdio.h>
+#include <math.h>
 
-using namespace std;
 
 int main()
 {
     double ganhoSalario, ganhoServicos, ganhoCapital, gastoMedico, gastoEducacao;
+    double impostoSalario, impostoServico, impostoCapital;
+    scanf ("%lf %lf %lf %lf %lf", &ganhoSalario, &ganhoServicos, &ganhoCapital, &gastoMedico, &gastoEducacao);
 
-    cout << "Renda anual com sal·rio: ";
-    cin >> ganhoSalario;
-    cout << "Renda anual com prestaÁ„o de serviÁo: ";
-    cin >> ganhoServicos;
-    cout << "Renda anual com ganho de capital: ";
-    cin >> ganhoCapital;
-    cout << "Gastos mÈdicos: ";
-    cin >> gastoMedico;
-    cout << "Gastos educacionais: ";
-    cin >> gastoEducacao;
+// 1 LEITURA DOS DADOS
 
+    printf ( "Renda anual com sal√°rio: \n");
+    scanf ("%lf",&ganhoSalario);
+    printf ( "Renda anual com presta√ß√£o de servi√ßo: \n");
+    scanf  ("%lf",&ganhoServicos);
+    printf ( "Renda anual com ganho de capital: \n");
+    scanf ("%lf",&ganhoCapital);
+    printf ( "Gastos m√©dicos: \n");
+    scanf ("%lf",&gastoMedico);
+    printf ( "Gastos educacionais: \n");
+    scanf ("%lf",&gastoEducacao);
+
+    // 2 CALCULOS CONSOLIDADO DE RENDA
+
+    double salarioMensal = ganhoSalario / 12.0;
+
+    if (salarioMensal < 3000.0)
+    {
+        impostoSalario = 0.0;
+    }
+    else if (salarioMensal <5000.0)
+    {
+        impostoSalario = ganhoSalario * 0.1;
+    }
+    else
+    {
+        impostoSalario = ganhoSalario * 0.2;
+    }
+    impostoServico = ganhoServicos * 0.15;
+    impostoCapital = ganhoCapital * 0.2;
+
+    // RELATORIO
+
+    printf ("RELAT√ìRIO DE IMPOSTO DE RENDA\n");    
+    printf ("CONSOLIDADO DE RENDA:");
+    printf ( "Imposto sobre salario: \n");
+    printf ( "Imposto sobre servicos: \n");
+    printf ( "Imposto sobre ganho de capital: \n");
+    
     return 0;
 }
