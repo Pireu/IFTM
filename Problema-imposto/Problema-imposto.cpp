1f3b62de99a3c98e04fe6fbac1e18d1c547418dc
Pireu <65513625+Pireu@users.noreply.github.com>
2020-05-27 17:28:43 -0300

    Deducoes

--- Problema-imposto/Problema-imposto.cpp
+++ Problema-imposto/Problema-imposto.cpp
@@ -7,47 +7,65 @@ int main()
 {
     double ganhoSalario, ganhoServicos, ganhoCapital, gastoMedico, gastoEducacao;
     double impostoSalario, impostoServico, impostoCapital;
-    scanf ("%lf %lf %lf %lf %lf", &ganhoSalario, &ganhoServicos, &ganhoCapital, &gastoMedico, &gastoEducacao);
+    double impostoBruto, maximoDedutivel, gastosDedutiveis;
+
+
 
 // 1 LEITURA DOS DADOS
 
-    printf ( "Renda anual com salário: \n");
+    printf ( "Renda anual com salario: ");
     scanf ("%lf",&ganhoSalario);
-    printf ( "Renda anual com prestação de serviço: \n");
+    printf ( "Renda anual com prestação de servico: ");
     scanf  ("%lf",&ganhoServicos);
-    printf ( "Renda anual com ganho de capital: \n");
+    printf ( "Renda anual com ganho de capital: ");
     scanf ("%lf",&ganhoCapital);
-    printf ( "Gastos médicos: \n");
+    printf ( "Gastos medicos: ");
     scanf ("%lf",&gastoMedico);
-    printf ( "Gastos educacionais: \n");
+    printf ( "Gastos educacionais: ");
     scanf ("%lf",&gastoEducacao);
 
     // 2 CALCULOS CONSOLIDADO DE RENDA
 
-    double salarioMensal = ganhoSalario / 12.0;
+    double   salarioMensal = (ganhoSalario / 12.0);
 
     if (salarioMensal < 3000.0)
     {
         impostoSalario = 0.0;
     }
-    else if (salarioMensal <5000.0)
+    else if (salarioMensal < 5000.0)
     {
-        impostoSalario = ganhoSalario * 0.1;
+        impostoSalario = (ganhoSalario * 0.1);
     }
     else
     {
-        impostoSalario = ganhoSalario * 0.2;
+        impostoSalario = (ganhoSalario * 0.2);
     }
-    impostoServico = ganhoServicos * 0.15;
-    impostoCapital = ganhoCapital * 0.2;
+    impostoServico = (ganhoServicos * 0.15);
+    impostoCapital = (ganhoCapital * 0.2);
+
+    // CALCULO DAS DEDUCOES
+
+    impostoBruto = impostoSalario + impostoServico + impostoCapital;
+
+    maximoDedutivel = impostoBruto * 0.3;
+
+    gastosDedutiveis = gastoEducacao + gastoMedico;
+
 
     // RELATORIO
 
-    printf ("RELATÓRIO DE IMPOSTO DE RENDA\n");    
-    printf ("CONSOLIDADO DE RENDA:");
-    printf ( "Imposto sobre salario: \n");
-    printf ( "Imposto sobre servicos: \n");
-    printf ( "Imposto sobre ganho de capital: \n");
-    
+    printf ("\n\nRELATÓRIO DE IMPOSTO DE RENDA\n\n");
+    printf ("CONSOLIDADO DE RENDA:\n");
+    printf ( "Imposto sobre salario: %.2lf\n", impostoSalario);
+    printf ( "Imposto sobre servicos: %.2lf\n", impostoServico);
+    printf ( "Imposto sobre ganho de capital: %.2lf\n", impostoCapital);
+
+    //DEDUCOES
+
+    printf("\n\nDEDUCOES:\n");
+    printf("Maximo dedutivel: %.2lf\n", maximoDedutivel);
+    printf("Gastos dedutiveis: %.2lf\n", gastosDedutiveis);
+
+
     return 0;
 }
